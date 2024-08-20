0e7a53a2505ed3abc2c8007531968a7e46467f0e
Shanawaz <135978935+Shanawaz002@users.noreply.github.com>
2024-08-20 20:05:14 +0530

    business marketing project

---
+++ navfun.js
@@ -0,0 +1,100 @@
+let auth= localStorage.getItem("log")||"";
+let get_data1 = () =>{
+
+    localStorage.setItem("pro_page_key",("earphone"));
+    window.location.href="./product1.html";
+
+};
+
+let get_data2 = () => {
+
+    localStorage.setItem("pro_page_key",("headphone"));
+    window.location.href="./product1.html";
+
+};
+
+let get_data3 = () => {
+
+    localStorage.setItem("pro_page_key",("wireless earbuds"));
+     window.location.href="./product1.html";
+
+};
+
+let get_data4 = () => {
+
+    localStorage.setItem("pro_page_key",("wireless headphones"));
+window.location.href="./product1.html";
+
+};
+
+let get_data5 = () => {
+
+    localStorage.setItem("pro_page_key",("earphone"));
+window.location.href="./product1.html";
+
+}
+
+let get_data6 = () => {
+
+    localStorage.setItem("pro_page_key",("work from home"));
+window.location.href="./product1.html";
+
+}
+
+
+
+
+//brand
+
+let getBrand1 = () => {
+
+    localStorage.setItem("pro_page_brand",("AUDIO-TECHNICA"));
+window.location.href="./product1.html";
+
+}
+
+let getBrand2 = () => {
+
+    localStorage.setItem("pro_page_brand",("SONY"));
+window.location.href="./product1.html";
+
+}
+let getBrand3 = () => {
+
+    localStorage.setItem("pro_page_brand",("BEYERDYNAMIC"));
+window.location.href="./product1.html";
+
+}
+let getBrand4 = () => {
+
+    localStorage.setItem("pro_page_brand",("HIFIMAN"));
+window.location.href="./product1.html";
+
+}
+let getBrand5 = () => {
+
+    localStorage.setItem("pro_page_brand",("SENNHEISER"));
+window.location.href="./product1.html";
+
+}
+
+//search------------->
+// document.getElementById('nav_search').addEventListener()
+let search = document.getElementById('nav_search')
+ function searchRes(search){
+    if(event.key === 'Enter'){
+        console.log('search:', search.value)
+        localStorage.setItem("pro_page_key",search.value);
+        window.location.href="./product1.html";
+    }
+    
+}
+
+// cart
+let redirect=()=>{
+    auth?window.location.href="./cart.html":window.location.href="./login.html" 
+}
+
+
+
+
---
+++ ticket.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+let data = JSON.parse(localStorage.getItem("userData")) || [];
+
+class User {
+  constructor(name,email, pass,last_name) {
+    this.name = name;
+this.last_name=last_name
+    this.email = email;
+    this.password = pass;
+  }
+  checkData = (email) => {
+    if (
+      this.name.length <= 0 ||
+   
+      this.email.length <= 0 ||
+      this.password.length <= 0
+    ) {
+      alert("fill the correct info");
+      return 0;
+    }
+    let flag = false;
+    data.forEach((el) => {
+      if (el.email == email) {
+        flag = true;
+        return flag;
+      }
+    });
+    return flag;
+  };
+}
+
+let form = document.querySelector("form");
+let addUser = (event) => {
+  event.preventDefault();
+  let name = form.name.value;
+  let last_name = form.last_name.value;
+  let email = form.email.value;
+  let phone = form.phone.value;
+  let url = form.url.value;
+  let user = new User(name,email,phone,url,last_name);
+  let flag = user.checkData(email);
+  if (flag) {
+    alert("Thanks for raise your ticket")
+    window.location.href="./index.html";
+   // window.location.reload();
+    return;
+  }
+  if (flag !== 0) {
+    data.push(user);
+    alert("Thanks for raise your ticket");
+    window.location.href="./index.html";
+    // localStorage.setItem("userData", JSON.stringify(data));
+   
+  }
+};
+form.addEventListener("submit", addUser);
